929fa61c9f84fdeb6637ce6045a62d63639383fa
Emanuel Bustos <fakuj305@gmail.com>
2026-03-19 21:39:33 -0300

    auth completed

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function DashboardPage() {
+  const { user, loading, logout } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/login");
+    }
+  }, [user, loading]);
+
+  if (loading) return <p className="text-white">Cargando...</p>;
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+      <h1 className="text-2xl font-bold mb-2">
+        Bienvenido, {user?.displayName}
+      </h1>
+      <p className="text-gray-400 mb-6">{user?.email}</p>
+      <button
+        onClick={logout}
+        className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition"
+      >
+        Cerrar sesión
+      </button>
+    </main>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function LoginPage() {
+  const { user, loading, loginWithGoogle } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Se ejecuta al montar el componente y tambien cada vez que [user, loading] cambien
+    if (!loading && user) {
+      // Si no esta cargando (osea que useAuth() termino de leer el token) y hay un usuario, entonces vas al dashboard
+      router.push("/dashboard");
+    }
+  }, [user, loading]);
+
+  return (
+    <main className="min-h-screen bg-black">
+      <div className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+        <h1 className="text-4xl font-bold mb-2">Inkoed</h1>
+        <p className="text-gray-400 mb-8">Tu canvas de codigo</p>
+        <button
+          onClick={loginWithGoogle}
+          className="bg-white text-black px-6 py-3 rounded-lg font-medium hover:bg-gray-200 transition"
+        >
+          Entrar con google
+        </button>
+      </div>
+    </main>
+  );
+}
